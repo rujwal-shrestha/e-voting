a537ad13dde23e1567ae6b1d8e6a37426f72c7e0
Ruzal <ruzal.sth8888@gmail.com>
2022-08-16 21:52:10 +0545

    SECOND

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "relaruru4",
+    password: "",
     database: "voting"
 });
 
